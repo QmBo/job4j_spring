0faa784328b4bdc2484ba0d30fb0740a02c29e26
Victor Egorov <qrioflat@gmail.com>
2020-05-29 16:28:30 +0300

    Продемонстрировать различные виды создания spring beans. [#230559]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,29 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * SpringDI
+ *
+ * @author Victor Egorov (qrioflat@gmail.com).
+ * @version 0.1
+ * @since 29.05.2020
+ */
+public class SpringDI {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Victor Egorov");
+        ui.add("Ivan Ivanov");
+        ui.print();
+    }
+}
